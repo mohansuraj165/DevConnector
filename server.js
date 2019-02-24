091d0842f7858a067f48c08109b6c6f1cb6a589b
mohansuraj165 <37024382+mohansuraj165@users.noreply.github.com>
2019-02-24 16:09:14 -0700

    Post and Profile API routes updated

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
-const port = process.env.port || 5000;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURI;
@@ -15,13 +20,18 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
-//connect to MongoBD
 
-app.get("/", (req, res) => res.send("Hello"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 //Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
